d4ca4ffdd1189120e6a164d9790958b58935e4b9
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-01-25 13:38:55 -0500

    update

--- front/src/components/notes/CreateNote.js
+++ front/src/components/notes/CreateNote.js
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 export default function CreateNote() {
+  const [note, setNote] = useState({
+    title: '',
+    content: '',
+    date: ''
+})
+const navigate = useNavigate()
+
+const onChangeInput = e => {
+    const {name, value} = e.target;
+    setNote({...note, [name]:value})
+}
+
+
+const createNote = async e => {
+    e.preventDefault()
+    try {
+        const token = localStorage.getItem('tokenStore')
+        if(token){
+            const {title, content, date} = note;
+            const newNote = {
+                title, content, date
+            }
+
+            await axios.post('/api/notes', newNote, {
+                headers: {Authorization: token}
+            })
+
+            return navigate.push('/')
+        }
+    } catch (err) {
+        window.location.href = "/";
+    }
+}
+
   return (
     <div className='create'>
       <h3>Create Note</h3>
